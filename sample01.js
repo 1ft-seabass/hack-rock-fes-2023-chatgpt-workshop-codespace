002d34af9461aec10e45b5f069b35bfb08f17609
1ft-seabass <seigo@1ft-seabass.jp>
2023-10-17 07:29:14 +0000

    更新

--- sample01.js
+++ sample01.js
@@ -7,8 +7,8 @@ const OpenAI = require("openai");
 // OpenAI の API を使うために上記の設定を割り当てて準備
 // 以後 openai というオブジェクトで使える
 const openai = new OpenAI({
-    apiKey: OPENAI_API_KEY
-})
+  apiKey: OPENAI_API_KEY
+});
 
 // async つきで実行
 // 外部の API (OpenAPI) とのやり取りが伴うので待つ処理が必要
@@ -17,10 +17,13 @@ async function main() {
   // ChatGPT API に実際にアクセス
   // https://platform.openai.com/docs/guides/gpt/chat-completions-api?lang=node.js
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "こんにちは!ChatGPTさん!" }],
-    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "user", content: "こんにちは!ChatGPTさん!" }  // ユーザーから「こんにちは!ChatGPTさん!」の質問
+    ],
+    model: "gpt-3.5-turbo", // モデルは gpt-3.5-turbo を今回使う。
   });
 
+  // 結果表示
   console.log(completion.choices[0].message.content);
 }
 
